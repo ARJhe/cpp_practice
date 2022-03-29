f84514189e622d8cb889b586d2da8a161494b994
ArJhe <meopkids@gmail.com>
2022-03-29 23:24:08 +0800

    20220329 update

---
+++ CppPrimer/2.4/reference_to_const.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    const int ci=1024;
+    double dval = 3.14;
+    const int temp = dval;
+    // int &ri = temp; error: cannot bind non-const lvalue reference of type 'int&' to an rvalue of type 'int'
+    return 0;
+}
+// Use this template always on leetcode to improve runtime.
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+   return 0;
+}();
+
---
+++ CppPrimer/2.4/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
+// Use this template always on leetcode to improve runtime.
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+   return 0;
+}();
+
